d8058db07c55491ba59d4db2efd943d5b7bd51ba
Daniel3886 <112687208+Daniel3886@users.noreply.github.com>
2023-10-02 13:20:19 +0100

    Add files via upload

---
+++ zad1.java
@@ -0,0 +1,9 @@
+public class zad1
+{
+	public static void main(String[] args)
+	{
+		String month = "lipiec";
+		int year = 2007;
+		System.out.println("Urodzilem sie w miesiacu " +month+ " w roku " +year+ ".");
+	}
+}
---
+++ zad3.java
@@ -0,0 +1,15 @@
+public class Sale
+{
+	public static void main(String[] args)
+	{
+	
+		double tax = 1.76;
+		double price = 29.75;
+		double total = 31.51;
+		
+		System.out.println("cena produktu wynosi" +price+ ".");
+		System.out.println("podatek wynosi" +tax+ ".");
+		System.out.println("laczna kwota wynosi" +total+ ".");
+		
+	}
+}//java zad3.java
